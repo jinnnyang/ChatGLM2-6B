16b9e41fd10874c5417b844a4ffcd58a9f4081a9
jinnyang <jinnyang@139.com>
2023-07-23 22:36:36 +0800

    Add Multi-GPU and network support.

--- web_demo.py
+++ web_demo.py
@@ -3,11 +3,11 @@ import gradio as gr
 import mdtex2html
 from utils import load_model_on_gpus
 
-tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm2-6b", trust_remote_code=True)
-model = AutoModel.from_pretrained("THUDM/chatglm2-6b", trust_remote_code=True).cuda()
+tokenizer = AutoTokenizer.from_pretrained("chatglm2-6b", trust_remote_code=True)
+# model = AutoModel.from_pretrained("THUDM/chatglm2-6b", trust_remote_code=True).cuda()
 # 多显卡支持，使用下面两行代替上面一行，将num_gpus改为你实际的显卡数量
-# from utils import load_model_on_gpus
-# model = load_model_on_gpus("THUDM/chatglm2-6b", num_gpus=2)
+from utils import load_model_on_gpus
+model = load_model_on_gpus("chatglm2-6b", num_gpus=2)
 model = model.eval()
 
 """Override Chatbot.postprocess"""
@@ -105,4 +105,4 @@ with gr.Blocks() as demo:
 
     emptyBtn.click(reset_state, outputs=[chatbot, history, past_key_values], show_progress=True)
 
-demo.queue().launch(share=False, inbrowser=True)
+demo.queue().launch(share=False, inbrowser=True, server_name="0.0.0.0", server_port=8086)
